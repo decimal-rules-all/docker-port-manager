2d5f9c538a6361df04612bfe3510af7444bb1355
dra <decimal.rules.all@gmail.com>
2024-01-04 23:23:33 +1300

    add docker port manager class

--- docker_port_manager/docker_port_manager.py
+++ docker_port_manager/docker_port_manager.py
@@ -9,19 +9,55 @@ CONFIG_FILE = 'config.v2.json'
 HOSTCONFIG_FILE = 'hostconfig.json'
 
 
-def list_ports(container_id: str) -> None:
-    """list all ports for specific container"""
-    with open(os.path.join(CONTAINER_PATH, container_id, CONFIG_FILE), encoding='utf-8') as file:
-        config = json.load(file)
+class DockerPortManager:
+    """docker port manager"""
 
-    exported_ports = config.get('Config', {}).get('ExposedPorts', {})
-    print(exported_ports)
+    def __init__(self, container_id: str) -> None:
+        self._container_id = container_id
+        self.container_config = self.load_container_config()
+        self.host_config = self.load_host_config()
 
-    with open(os.path.join(CONTAINER_PATH, container_id, HOSTCONFIG_FILE), encoding='utf-8') as file:
-        hostconfig = json.load(file)
+    def load_container_config(self) -> dict:
+        """load container config"""
+        config_path = os.path.join(CONTAINER_PATH, self._container_id, CONFIG_FILE)
+        with open(config_path, encoding='utf-8') as file:
+            return json.load(file)
 
-    ports = hostconfig.get('PortBindings', {})
-    print(ports)
+    def load_host_config(self) -> dict:
+        """load host config"""
+        config_path = os.path.join(CONTAINER_PATH, self._container_id, HOSTCONFIG_FILE)
+        with open(config_path, encoding='utf-8') as file:
+            return json.load(file)
+
+    def list_exported_ports(self) -> dict:
+        """list all exported ports"""
+        return self.container_config.get('Config', {}).get('ExposedPorts', {})
+
+    def list_port_bindings(self) -> dict:
+        """list all port bindings"""
+        return self.host_config.get('PortBindings', {})
+
+    def add_exported_port(self, port: int) -> None:
+        """add exported port"""
+        exported_port = {str(port) + '/tcp': {}}
+        self.container_config['Config']['ExposedPorts'].update(exported_port)
+
+    def add_port_binding(self, container_port: int, host_port: int) -> None:
+        """add port binding"""
+        port_binding = {str(container_port) + '/tcp': [{'HostIp': '', 'HostPort': str(host_port)}]}
+        self.host_config['PortBindings'].update(port_binding)
+
+    def save_container_config(self) -> None:
+        """save container config"""
+        config_path = os.path.join(CONTAINER_PATH, self._container_id, CONFIG_FILE)
+        with open(config_path, 'w', encoding='utf-8') as file:
+            json.dump(self.container_config, file)
+
+    def save_host_config(self) -> None:
+        """save host config"""
+        config_path = os.path.join(CONTAINER_PATH, self._container_id, HOSTCONFIG_FILE)
+        with open(config_path, 'w', encoding='utf-8') as file:
+            json.dump(self.host_config, file)
 
 
 if __name__ == '__main__':
@@ -30,4 +66,6 @@ if __name__ == '__main__':
                         type=str)
     args = parser.parse_args()
 
-    list_ports(args.container_id)
+    dpm = DockerPortManager(args.container_id)
+    print(dpm.list_exported_ports())
+    print(dpm.list_port_bindings())
