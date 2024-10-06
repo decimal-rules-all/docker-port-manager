fee64288b9b503bbb6225ad6ac7cb60ab314f7ba
dra <decimal.rules.all@gmail.com>
2024-10-06 23:33:48 +1300

    add tooltip to port cell

--- web/src/components/ContainerTable/PortCell.tsx
+++ web/src/components/ContainerTable/PortCell.tsx
@@ -1,4 +1,4 @@
-import { Chip } from "@mui/material";
+import { Chip, Tooltip } from "@mui/material";
 import { HostPort } from "types/HostPort";
 import { PortBinding } from "types/PortBinding";
 
@@ -11,7 +11,16 @@ const PortCell = ({ portBindings }: PortCellProps) => {
         <>
             {portBindings.map((portBinding) => (
                 portBinding.host_ports.map((hostPort: HostPort) => (
-                  <Chip sx={{marginRight: 0.5}} variant="outlined" label={`${portBinding.exposed_port.port}:${hostPort.host_port}/${portBinding.exposed_port.protocol}`} />
+                    <Tooltip title={
+                        <>
+                            Portocol: {portBinding.exposed_port.protocol} <br />
+                            Exposed Port: {portBinding.exposed_port.port} <br />
+                            Host Port: {hostPort.host_port} <br />
+                            Host IP: {hostPort.host_ip}
+                        </>
+                    } arrow>
+                        <Chip sx={{marginRight: 0.5}} variant="outlined" label={`${portBinding.exposed_port.port}:${hostPort.host_port}/${portBinding.exposed_port.protocol}`} />
+                    </Tooltip>
                 ))
             ))}
         </>
