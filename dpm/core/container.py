f4d35eeabd70e45a8c942b6072cddb6ab3c996a7
dra <decimal.rules.all@gmail.com>
2024-10-09 23:45:27 +1300

    add exported port management

--- dpm/core/container.py
+++ dpm/core/container.py
@@ -1,6 +1,7 @@
 import os
 import json
 
+from enum import Enum
 from typing import List
 
 
@@ -16,11 +17,31 @@ def load_container_config(container_id: str) -> dict:
         return json.load(file)
 
 
+def save_container_config(container_id: str, container_config: dict) -> None:
+    """save container config"""
+    config_path = os.path.join(CONTAINER_PATH, container_id, CONFIG_FILE)
+    with open(config_path, 'w', encoding='utf-8') as file:
+        json.dump(container_config, file)
+
+
 def load_host_config(container_id: str) -> dict:
     """load host config"""
     config_path = os.path.join(CONTAINER_PATH, container_id, HOSTCONFIG_FILE)
     with open(config_path, encoding='utf-8') as file:
         return json.load(file)
+
+
+def save_host_config(container_id: str, host_config: dict) -> None:
+    """save host config"""
+    config_path = os.path.join(CONTAINER_PATH, container_id, HOSTCONFIG_FILE)
+    with open(config_path, 'w', encoding='utf-8') as file:
+        json.dump(host_config, file)
+
+
+class Protocol(Enum):
+    """Enum for supported protocol"""
+    TCP = 'tcp'
+    UDP = 'udp'
 
 
 class Container():
@@ -38,6 +59,24 @@ class Container():
     def get_exported_ports(self) -> dict:
         """get exported ports"""
         return self.container_config.get('Config', {}).get('ExposedPorts', {})
+
+    def is_exported_port_exist(self, port: int, protocol: Protocol = Protocol.TCP) -> bool:
+        """detemine if container exports the port"""
+        exported_ports = self.get_exported_ports()
+        return "/".join([str(port), str(protocol.value)]) in exported_ports.keys()
+
+    def add_exported_port(self, port: int, protocol: Protocol = Protocol.TCP) -> None:
+        """add container exported port"""
+        if self.is_exported_port_exist(port, protocol):
+            return
+
+        if 'Config' not in self.container_config:
+            self.container_config['Config'] = {}
+        if 'ExposedPorts' not in self.container_config['Config']:
+            self.container_config['Config']['ExposedPorts'] = {}
+
+        exported_port = {"/".join([str(port), str(protocol.value)]): {}}
+        self.container_config['Config']['ExposedPorts'].update(exported_port)
 
     def get_port_bindings(self) -> dict:
         """get port bindings"""
--- dpm/tests/unit/core/test_container.py
+++ dpm/tests/unit/core/test_container.py
@@ -41,6 +41,12 @@ def test_load_container_config(mock_open, mock_json_load, container_config):
     mock_open.assert_called_once()
     assert config == container_config
 
+@mock.patch("dpm.core.container.json.dump")
+@mock.patch("dpm.core.container.open")
+def test_save_container_config(mock_open, mock_json_dump, container_config):
+    container_core.save_container_config('container_id', container_config)
+    mock_json_dump.assert_called_once_with(container_config, mock.ANY)
+
 @mock.patch("dpm.core.container.json.load")
 @mock.patch("dpm.core.container.open")
 def test_load_host_config(mock_open, mock_json_load, host_config):
@@ -48,6 +54,12 @@ def test_load_host_config(mock_open, mock_json_load, host_config):
     config = container_core.load_container_config('container_id')
     mock_open.assert_called_once()
     assert config == host_config
+
+@mock.patch("dpm.core.container.json.dump")
+@mock.patch("dpm.core.container.open")
+def test_save_host_config(mock_open, mock_json_dump, host_config):
+    container_core.save_host_config('container_id', host_config)
+    mock_json_dump.assert_called_once_with(host_config, mock.ANY)
 
 
 class TestContainer:
@@ -65,6 +77,28 @@ class TestContainer:
         mock_json_load.return_value = container_config
         container = container_core.Container('container_id')
         assert container.get_exported_ports() == {'80/tcp': {}, '443/tcp': {}, '53/udp': {}}
+
+    @mock.patch("dpm.core.container.json.load")
+    @mock.patch("dpm.core.container.open")
+    def test_is_exported_port_exist_negative(self, _, mock_json_load, container_config):
+        mock_json_load.return_value = container_config
+        container = container_core.Container('container_id')
+        assert not container.is_exported_port_exist(8080)
+
+    @mock.patch("dpm.core.container.json.load")
+    @mock.patch("dpm.core.container.open")
+    def test_is_exported_port_exist_positive(self, _, mock_json_load, container_config):
+        mock_json_load.return_value = container_config
+        container = container_core.Container('container_id')
+        assert container.is_exported_port_exist(80)
+
+    @mock.patch("dpm.core.container.json.load")
+    @mock.patch("dpm.core.container.open")
+    def test_add_exported_port(self, _, mock_json_load, container_config):
+        mock_json_load.return_value = container_config
+        container = container_core.Container('container_id')
+        container.add_exported_port(8080)
+        assert '8080/tcp' in container.get_exported_ports()
 
     @mock.patch("dpm.core.container.json.load")
     @mock.patch("dpm.core.container.open")
